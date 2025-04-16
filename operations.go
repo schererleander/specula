6c42060c7ef28c50be768ef70069fa5cce5b97dd
schererleander <leander@schererleander.de>
2025-04-16 23:07:44 +0200

    add created timestamp, cleanup getDescription

--- operations.go
+++ operations.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"os"
-	"os/user"
-	"os/exec"
-	"syscall"
 	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+	"time"
+
+	"golang.org/x/sys/unix"
 )
 
 func (m *model) populateFromInfo(info os.FileInfo) {
-	stat := info.Sys().(*syscall.Stat_t)
-
+	var stat unix.Stat_t
+	if err := unix.Stat(m.path, &stat); err != nil {
+		m.error = err.Error()
+		return
+	}
 	m.filename = info.Name()
 	m.size = info.Size()
-	m.lastModified = info.ModTime().String()
+	birth := time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec))
+	m.created = birth.Format("2006-01-02 15:04")
+	m.lastModified = info.ModTime().Format("2006-01-02 15:04")
 	m.permission = info.Mode().String()
-
 	u, err := user.LookupId(fmt.Sprintf("%d", stat.Uid))
 	if err != nil {
 		m.error = err.Error()
@@ -25,30 +32,18 @@ func (m *model) populateFromInfo(info os.FileInfo) {
 }
 
 func (m *model) getDescription(path string) {
-	output, err := exec.Command("file", path).Output()
+	out, err := exec.Command("file", "-b", "--separator=,", path).Output()
 	if err != nil {
 		m.error = err.Error()
 		return
 	}
-	m.description = string(output)
-}
 
-func (m *model) String() string {
-	return fmt.Sprintf(`
-Path:          %s
-Owner:         %s
-Size:          %d bytes
-Permissions:   %s
-Created:       %s
-Last Modified: %s
-File Type:     %s
-`,
-		m.path,
-		m.owner,
-		m.size,
-		m.permission,
-		m.created,
-		m.lastModified,
-		m.description,
-	)
+	desc := strings.TrimSpace(string(out))
+	parts := strings.Split(desc, ",")
+
+	for i := range parts {
+		parts[i] = strings.TrimSpace(parts[i])
+	}
+
+	m.description = strings.Join(parts, "\n")
 }
